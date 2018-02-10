6dbdb818ece7446b86d51760d38e20b5cf82b892
Pedro Madruga <pedro@occam.ooo>
2018-02-10 12:36:40 +0100

    react-native-camera

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 
 import AR from './src/AR';
+import Scan from './src/Scan';
 
+// AppRegistry.registerComponent('Owl', () => Scan);
 AppRegistry.registerComponent('Owl', () => AR);
